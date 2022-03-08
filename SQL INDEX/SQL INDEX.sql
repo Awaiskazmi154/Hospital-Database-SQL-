758479ad601d52d7f5512d32d833e22fc30b9bd6
Syed Awais Kazmi <75107223+Awaiskazmi154@users.noreply.github.com>
2022-03-08 16:05:53 +0500

    Add files via upload

---
+++ SQL INDEX/SQL INDEX.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS SQL_INDEX (
+    `CREATE_UNIQUE_INDEX_IX_StaffMember_Name` VARCHAR(40) CHARACTER SET utf8
+);
+INSERT INTO SQL_INDEX VALUES
+    ('ON StaffMember_info (member_name)'),
+    ('SELECT member_name FROM StaffMember_info'),
+    ('WHERE member_name LIKE ''o%'';');
